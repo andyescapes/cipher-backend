0fe2247752a990a091795fba35f3ab2f595da08a
unknown <cdd>
2021-04-10 23:27:21 +1000

    add cors

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 
 const deciphers = require("./caesar.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(`
